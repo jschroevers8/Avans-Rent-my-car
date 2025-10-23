acbf0c0b0ccf3e79f23331d1206861b7c2991c6c
KoenVergeer123 <D290735@edu.curio.nl>
2025-10-23 22:19:25 +0200

    Update Car extra image

---
+++ src/main/kotlin/rmc/presentation/dto/car/UpdateCar.kt
@@ -0,0 +1,22 @@
+package rmc.presentation.dto.car
+
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+import rmc.domain.entities.BodyType
+import rmc.domain.entities.FuelType
+import rmc.presentation.dto.image.CreateCarImage
+
+@Serializable
+data class UpdateCar(
+    val id: Int? = null,
+    val fuelType: FuelType,
+    val userId: Int,
+    val bodyType: BodyType,
+    val brand: String,
+    val model: String,
+    val modelYear: String,
+    val licensePlate: String,
+    val mileage: String,
+    val createdStamp: LocalDateTime,
+    val carImages: List<CreateCarImage> = emptyList(),
+)
